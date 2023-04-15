93415901a35d8550361d21acf3aa65235c9b6338
chand1012 <chandler@multisiglabs.org>
2023-04-15 12:17:38 -0400

    Remove unused code

--- main.py
+++ main.py
@@ -21,9 +21,11 @@ bot = commands.Bot(intents=intents)
 db = DB()
 db.login(os.getenv('DB_USERNAME'), os.getenv('DB_PASSWORD'))
 
+
 @bot.event
 async def on_ready():
     logger.info(f'We have logged in as {bot.user}')
+
 
 @bot.event
 async def on_message(message: Message):
@@ -58,44 +60,6 @@ async def on_message(message: Message):
     except Exception as e:
         logger.error(e)
         await m.edit(content='Error generating summary. If this is a valid video, please try again later.')
-
-# @bot.slash_command(name="yt", description="Get a summary of a YouTube video.", guild_ids=[int(TESTING_GUILD_ID)])
-# async def summary_command(
-#     interaction: Interaction,
-#     video: str = SlashOption(description="The URL of a YouTube video.", required=True)
-# ):
-#     videoId = get_youtube_video_id(video)
-#     if not videoId:
-#         await interaction.send('Invalid YouTube video URL.')
-#         return
-#     # defer the message
-#     await interaction.response.defer()
-#     logger.info(f'Getting transcript for {videoId}...')
-#     try:
-#         resp = db.get_transcript(videoId)
-#         transcript = resp['transcript']
-#         title = resp['title']
-#         summary = None
-#         try:
-#             logger.info('Checking if summary already exists...')
-#             summary = db.get_summary(videoId)
-#         except:
-#             pass
-#         if summary:
-#             logger.info('Summary already exists. Posting...')
-#             await interaction.send(f'Summary for "{title}"\n\n{summary.summary}')
-#             return
-#         logger.info('Generating summary...')
-#         start = time.time()
-#         summary = await chat_gpt_request(transcript)
-#         end = time.time()
-#         logger.info(f'Took {round(end-start, 2)}s to generate summary.')
-#         await interaction.send(f'Summary for "{title}"\n\n{summary}')
-#         logger.info(f'Posting summary to db...')
-#         db.post_summary(videoId, summary)
-#     except Exception as e:
-#         logger.error(e)
-#         await interaction.send('Error generating summary. If this is a valid video, please try again later.')
 
 
 bot.run(os.getenv('DISCORD_BOT_TOKEN'))
